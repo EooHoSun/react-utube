fc07648f241118c32957574105e693cb6b18be18
sun.kist <sun.kist@kakaocorm.com>
2021-06-30 16:08:28 +0900

    mobx 적용을 위한 커밋

--- src/components/video_list/video_list.tsx
+++ src/components/video_list/video_list.tsx
@@ -2,16 +2,30 @@ import React from 'react';
 import VideoItem from '../video_item/video_item';
 import styles from '../video_list/video_list.module.css';
 import { VideoListPropsType } from 'app';
+import { observer } from 'mobx-react';
+import { observable } from 'mobx';
 
+@observer
+class VideoList extends React.Component<VideoListPropsType>{
+    videos : Array<any> = [];
 
-const VideoList   : React.FunctionComponent<VideoListPropsType>  = ({videoList, onVideoClick, display}) => {
-    return (
-        <ul className={styles.videos}>
-            {
-                videoList && videoList.map((video) => <VideoItem key={video.id} video={video} onVideoClick={onVideoClick} display={display}/>)
-            }
-        </ul>            
+    constructor(props : VideoListPropsType){
+        super(props);
+        this.videos = props.videoList;
+    }
+    
+    render(){
+        const {videoList, display, onVideoClick} = this.props;
+        
+        return (
+            <ul className={styles.videos}>
+                {
+                    videoList && videoList.map((video) => <VideoItem key={video.id} video={video} onVideoClick={onVideoClick} display={display}/>)
+                }
+            </ul>            
         );
+
+    };
 }
 
 
